c62d2ad5d266aea0ad882db2f932de2251cab685
viktor <viktorwv@icloud.com>
2023-10-27 10:41:04 +0200

    Eksempel kode fra onenote

--- Bankkonto-oppgave/main.py
+++ Bankkonto-oppgave/main.py
@@ -2,9 +2,11 @@ from konto import Bankkonto, BSUKonto, Sparekonto
 
 
 def main() -> None:
-    bankkonto = Bankkonto("Viktor", "1920.21.34098", 1500)
-    bankkonto.uttak(2000)
-    bankkonto.innskudd(500)
+    bankkonto = Bankkonto("Viktor", "1920.21.34098", 0)
+    bankkonto.innskudd(1000)
+    bankkonto.innskudd(2000)
+    bankkonto.uttak(1500)
+    bankkonto.uttak(2500)
     print(bankkonto)
 
 
